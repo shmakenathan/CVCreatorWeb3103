8615122e54f6d48e5c5801301118dc87b9d8d211
Nathan <nathan.chicha.openclassroom@gmail.com>
2021-12-10 12:28:25 +0100

    login

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,13 +1,39 @@
 import Fluent
 import Vapor
 
+struct LoginResponse: Content {
+    let isSuccess: Bool
+}
+
+struct LoginRequestBody: Content {
+    let username: String
+    let password: String
+}
+
+
 func routes(_ app: Application) throws {
-    app.get { req in
+    
+    // MARK:
+    app.get("") { req in
         return req.view.render("index", ["title": "Hello Vapor!"])
     }
 
-    app.get("hello") { req -> String in
-        return "Hello, world!"
+    
+    
+    app.post("api", "v1", "login") { req -> LoginResponse in
+        guard let bodyData = req.body.data else {
+            throw Abort(.badRequest)
+        }
+
+        
+        let decodedData = try JSONDecoder().decode(LoginRequestBody.self, from: bodyData)
+        
+        print(decodedData.password)
+        let isValidUser = decodedData.username == "Nathan123"
+        let isValidPassword = decodedData.password == "password123"
+        let isSuccessLogin = isValidUser && isValidPassword
+        
+        return LoginResponse(isSuccess: isSuccessLogin)
     }
 
     try app.register(collection: TodoController())
